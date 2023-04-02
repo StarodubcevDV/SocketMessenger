d840fdffdf1d57add990cc04ee7417b4788865a3
StarodubcevDV <uswes123@yandex.ru>
2023-04-02 21:25:36 +0500

    feat(rooms): add implementation of rooms

--- client.py
+++ client.py
@@ -1,40 +1,42 @@
-import re
 import socket
 import json
-import threading
 import time
+import random
+import multiprocessing
+
 
 SERVER_ADDRESS = ('localhost', 9090)
+
+STOP_RECEIVING = False
+
+
+def receive_msg(sock_):
+    while True:
+        server_response, addr = sock_.recvfrom(1024)
+        server_response_message = server_response.decode("utf-8")
+        print("Server > " + server_response_message)
+        time.sleep(0.1)
 
 
 class User:
     def __init__(self, name_):
         self.__name = name_
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind(("127.0.0.1", 0))
-        # self.sock.setblocking(False)
+        self.sock.bind(("127.0.0.1", random.randint(0, 500)))   # Problem in socket port (same for all)
         self._current_room = None
         self._rooms = []
 
-    def connect_room(self, room):
-        self._current_room = room
-        self._rooms.append(self._current_room)
-
-    def receive_msg(self):
-        while True:
-            server_response, addr = self.sock.recvfrom(1024)
-            server_response_message = server_response.decode("utf-8")
-            print("Server > " + server_response_message)
-            time.sleep(0.1)
+    def get_rooms(self):
+        return self._rooms
 
     def chatting(self):
-        receiving_thread = threading.Thread(target=self.receive_msg)
+        receiving_thread = multiprocessing.Process(target=receive_msg, args=(self.sock,))
         receiving_thread.start()
         print('Start messaging: ')
         while True:
             message = input()
 
-            message_dict = {'name': self.__name, 'message': message}
+            message_dict = {'name': self.__name, 'room': self._current_room, 'message': message}
             user_encode_message = json.dumps(message_dict).encode('utf-8')
 
             try:
@@ -45,14 +47,39 @@ class User:
 
             if message == '!exit':
                 self.disconnect_room()
-                receiving_thread.join()
+                receiving_thread.terminate()
                 return 0
 
     def create_room(self, room_name):
+        message_dict = {'name': self.__name, 'room': room_name, 'message': '!create'}
+        user_encode_message = json.dumps(message_dict).encode('utf-8')
+        self.sock.sendto(user_encode_message, SERVER_ADDRESS)
+
+        server_response, addr = self.sock.recvfrom(1024)
+        server_response_message = server_response.decode("utf-8")
+        if server_response_message == 'created':
+            self._rooms.append(self._current_room)
+            print(f'{room_name} created!')
+        elif server_response_message == 'connected':
+            print('Room with the same name already exist, connecting.')
+        self._current_room = room_name
+
+    def connect_room(self, room_name):
+        message_dict = {'name': self.__name, 'room': room_name, 'message': '!connect'}
+        user_encode_message = json.dumps(message_dict).encode('utf-8')
+        self.sock.sendto(user_encode_message, SERVER_ADDRESS)
+        server_response, addr = self.sock.recvfrom(1024)
+        server_response_message = server_response.decode("utf-8")
+
+        if server_response_message == 'connected':
+            print(f'Connected to {room_name}!')
+        elif server_response_message == 'created':
+            self._rooms.append(room_name)
+            print('Room with the same name doesnt exist, creating.')
         self._current_room = room_name
 
     def disconnect_room(self):
-        print(f'Disconnected')
+        print(f'Disconnected from {self._current_room}')
         self._current_room = None
 
 
@@ -62,7 +89,7 @@ if __name__ == '__main__':
 
     while True:
         try:
-            option = input('[connect] the existing room or [create] your own:')
+            option = input('[connect] the existing room or [create] your own: ')
             if option == 'connect':
                 room_address = input('Enter room name: ')
                 user.connect_room(room_address)
@@ -73,4 +100,5 @@ if __name__ == '__main__':
                 user.chatting()
         except Exception as e:
             print(e)
+            continue
 
--- server.py
+++ server.py
@@ -1,16 +1,43 @@
 import socket
 import time
-import json
 import ast
 
+ROOMS = []
 
-USERS_LIST = []
 
+class Room:
+    def __init__(self, name, sock_):
+        self.room_name = name
+        self.user_list = {}
+        self.sock = sock_
 
-def send_data_to_all_users(sock_send, sender, data_to_send):
-    for user in USERS_LIST:
-        if sender != user:
-            sock_send.sendto(data_to_send, user)
+    def get_name(self):
+        return self.room_name
+
+    def get_users_names(self):
+        return list(self.user_list.keys())
+
+    def get_users_addresses(self):
+        return list(self.user_list.values())
+
+    def add_user(self, user_name, user_address):
+        self.user_list[user_name] = user_address
+        cur_time = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
+        connection_data = f'{user_name} connected at {cur_time}'.encode('UTF-8')
+        self.send_data_to_all_users(user_address, connection_data)
+        print(connection_data.decode('UTF-8'))
+
+    def disconnect_user(self, user_name):
+        if len(self.get_users_names()) - 1 != 0:
+            cur_time = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
+            connection_data = f'{user_name} disconnected at {cur_time}'.encode('UTF-8')
+            self.send_data_to_all_users(self.user_list[user_name], connection_data)
+        del self.user_list[user_name]
+
+    def send_data_to_all_users(self, sender, data_to_send):
+        for user in self.get_users_addresses():
+            if sender != user:
+                self.sock.sendto(data_to_send, user)
 
 
 def parse_received_data(received_data):
@@ -22,32 +49,41 @@ def create_response(decoded_data):
     return f'{decoded_data["name"]}: {decoded_data["message"]}'.encode('UTF-8')
 
 
+def chatting(room_, parsed_data, user_addr_):
+    try:
+        if parsed_data['message'] == '!exit':
+            room_.disconnect_user(parsed_data['name'])
+        elif parsed_data['message'] == '!users':
+            users_data = f'{room_.get_users_names()}'.encode('UTF-8')
+            room_.sock.sendto(users_data, user_addr_)
+        else:
+            room_.send_data_to_all_users(user_addr_, create_response(parsed_data))
+    except:
+        print("Room stopped")
+        return -1
+
+
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('', 9090))
 
     while True:
-        try:
-            data, user_addr = sock.recvfrom(1024)
+        data, user_addr = sock.recvfrom(1024)
 
-            if user_addr not in USERS_LIST:
-                USERS_LIST.append(user_addr)
-                cur_time = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
-                connection_data = f'{parse_received_data(data)["name"]} connected at {cur_time}'.encode('UTF-8')
-                send_data_to_all_users(sock, user_addr, connection_data)
-                print(connection_data.decode('UTF-8'))
+        parsed_data = parse_received_data(data)
 
-            if parse_received_data(data)['message'] == '!exit':
-                print('ololo disc')
-                USERS_LIST.remove(user_addr)
-                cur_time = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
-                connection_data = f'{parse_received_data(data)["name"]} disconnected at {cur_time}'.encode('UTF-8')
-                send_data_to_all_users(sock, user_addr, connection_data)
-            elif parse_received_data(data)['message'] == '!users':
-                users_data = f'{USERS_LIST}'.encode('UTF-8')
-                sock.sendto(users_data, user_addr)
+        if parsed_data['message'] == '!create' or parsed_data['message'] == '!connect':
+            if parsed_data['room'] not in [room_.get_name() for room_ in ROOMS]:
+                ROOMS.append(Room(parsed_data['room'], sock))
+                ROOMS[-1].add_user(parsed_data['name'], user_addr)
+                sock.sendto('created'.encode('UTF-8'), user_addr)
+                # chatting(ROOMS[-1], parsed_data, user_addr)
             else:
-                send_data_to_all_users(sock, user_addr, create_response(parse_received_data(data)))
-        except:
-            print("Server stopped")
-            break
+                room = ROOMS[[room_.get_name() for room_ in ROOMS].index(parsed_data['room'])]
+                if parsed_data['name'] not in room.get_users_names():
+                    room.add_user(parsed_data['name'], user_addr)
+                    sock.sendto('connected'.encode('UTF-8'), user_addr)
+                    # chatting(room, parsed_data, user_addr)
+        else:
+            room = ROOMS[[room_.get_name() for room_ in ROOMS].index(parsed_data['room'])]
+            chatting(room, parsed_data, user_addr)
